558f7150e0fb97deb90c25bb106255107f2686fa
santosh <santoshhasabe567@gmail.com>
2023-09-11 23:56:59 +0530

    Changes

--- calculator.js
+++ calculator.js
@@ -1,20 +1,33 @@
-
 let displayValue = '';
-function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById('display').value = displayValue;
-}
+        
+        function appendToDisplay(value) {
+            displayValue += value;
+            document.getElementById('display').value = displayValue;
+        }
 
-function clearDisplay() {
-    displayValue = '';
-    document.getElementById('display').value = '';
-}
+        function clearDisplay() {
+            displayValue = '';
+            document.getElementById('display').value = '';
+        }
 
-function calculateResult() {
-    try {
-        displayValue = eval(displayValue);
-        document.getElementById('display').value = displayValue;
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
-    }
-}
+        function deleteSingle() {
+            var display = document.getElementById('display');
+            var currentValue = displayValue;
+                displayValue = currentValue.slice(0, -1);
+                document.getElementById('display').value = displayValue;
+            
+        }
+
+        function calculateResult() {
+            var currentValue = displayValue;
+            currentValue = currentValue.replace(/(\d+)%/g, function(match, p1) {
+                return parseFloat(p1) / 100;
+            });
+        
+            try {
+                displayValue = eval(currentValue);
+                document.getElementById('display').value = displayValue;
+            } catch (error) {
+                document.getElementById('display').value = 'Error';
+            }
+        }
